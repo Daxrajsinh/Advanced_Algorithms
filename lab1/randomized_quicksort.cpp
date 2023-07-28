2f559b4bbb84f3f00715a1675494928c237afc70
Daxrajsinh <107194145+Daxrajsinh@users.noreply.github.com>
2023-07-28 15:23:38 +0530

    Update randomized_quicksort.cpp

--- lab1/randomized_quicksort.cpp
+++ lab1/randomized_quicksort.cpp
@@ -5,94 +5,63 @@ ID : 21CEUES118
 Sem - V
 */
 
-// Ford-Fulkarson Algorithm
 #include <bits/stdc++.h>
 using namespace std;
 
-bool BFS(vector<vector<int>> &rGraph, int s, int t, vector<int> &parent) {
-    int n = rGraph.size();
-    vector<bool> vis(n, false);
-    
-    queue<int> q;
-    q.push(s);
-    
-    vis[s] = 1; //Making source visited.
-    parent[s] = -1;  //No parent of Source.
-    
-    while(! q.empty()) {
-        int u = q.front();
-        q.pop();
-        for(int v=0; v<n; v++) {
-            if(!vis[v] and rGraph[u][v] > 0) {
-                if(v == t) {
-                    parent[v] = u; 
-                    return true;  // Returns true if there is path from source to sink.
-                }
-                q.push(v);
-                vis[v] = 1;   //Node is Visited.
-                parent[v] = u;
-            }
-        }
-    }
-    return false;
+int comp = 0, swaps = 0;
+
+int partition(int a[], int p, int r) {
+   int x = a[r];
+   int i = p-1;
+   int j;
+  
+   for(j=p; j<=r-1; j++) {
+       comp++;
+       if(a[j] <= x) {
+           i++;
+           if(a[i] != a[j]) {
+               swaps++;
+               swap(a[i], a[j]);
+           }
+       }
+   }
+   swap(a[i+1], a[r]);
+   return i+1;
 }
 
-int fordfulkerson(vector<vector<int>> &G, int s, int t) {
-    int n = G.size();
-    vector<vector<int>> residualGraph(n, vector<int>(n, 0));
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            residualGraph[i][j] = G[i][j]; // Initialize residual graph with original capacities
-        }
-    }
+int randomized_partition(int a[], int p, int r) {
+   srand(time(NULL));
+   int i = p + (rand() % (r - p + 1)); //range from p to r.
+   swap(a[i], a[r]);
+   return partition(a, p, r);
+}
 
-    vector<int> parent(n);
 
-    int max_flow = 0;
-
-    while (BFS(residualGraph, s, t, parent)) {
-        int bottleneck = INT_MAX;
-        
-        //Finding Bottleneck Capacity
-        for (int v = t; v != s; v = parent[v]) {
-            int u = parent[v];
-            path_flow = min(bottleneck, residualGraph[u][v]);
-        }
-
-        for (int v = t; v != s; v = parent[v]) {
-            int u = parent[v];
-            residualGraph[u][v] -= bottleneck;
-            residualGraph[v][u] += bottleneck;
-        }
-
-        max_flow += bottleneck;
-    }
-
-    return max_flow;
+void randomized_QuickSort(int a[], int p, int r) {
+   if(p < r) {
+       int q = randomized_partition(a,p,r);
+       randomized_QuickSort(a, p, q-1);
+       randomized_QuickSort(a, q+1, r);
+   }
 }
 
 
 int main() {
-    int n;
-    cout << "Enter no. of nodes :\n";
-    cin >> n;
-    
-    vector<vector<int>> graph(n , vector<int> (n, 0));
-    
-    cout << "Provide Graph such that value of g[i][j] is flow value b/w i -> j:\n";
-    
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cin >> graph[i][j];
-        }
-    }
-    
-    int source, sink;
-    cout << "Provide source and sink:\n";
-    cin >> source >> sink;
-    
-    cout << "Min. flow in network is : " << fordfulkerson(graph, source, sink);
-    
-    return 0;
+   int n;
+   cin >> n;
+  
+   int a[n];
+  
+   for(int i=0; i<n; i++) {
+       cin >> a[i];
+   }
+  
+   randomized_QuickSort(a, 0, n-1);
+  
+   for(int i=0; i<n; i++) cout << a[i] << " ";
+  
+   cout << "\nNo. of comparisons :" << comp << endl;
+   cout << "No. of swaps :" << swaps << endl;
+   return 0;
 }
