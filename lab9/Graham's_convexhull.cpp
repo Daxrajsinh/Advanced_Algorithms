7062af31c2d0bc5df42772c6fab537dcd37d2030
Daxrajsinh <107194145+Daxrajsinh@users.noreply.github.com>
2023-09-19 10:53:42 +0530

    Update Graham's_convexhull.cpp

--- lab9/Graham's_convexhull.cpp
+++ lab9/Graham's_convexhull.cpp
@@ -1,5 +1,3 @@
-// ********** REMAINING !!!!!!!! ************ //
-
 /*
 Name : Daxrajsinh Jadeja
 Roll No. : CE052
@@ -8,7 +6,7 @@ Sem - V
 */
 
 //Graham's algorithm to find Convex Hull points.
-//Time complexity : O(nlogn)
+//Time complexity : O(nlogn) - (constant)
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -30,21 +28,21 @@ bool compareByFirstElement(const pair<float, Point>& a, const pair<float, Point>
         int dy1 = a.second.y - p0.y;
         int dx2 = b.second.x - p0.x;
         int dy2 = b.second.y - p0.y;
-        
+       
         return (dx1 * dx1 + dy1 * dy1) < (dx2 * dx2 + dy2 * dy2);
     }
 }
 
 float polar_angle(Point p0, Point p1) {
     //handling these cases differently, coz they're giving wrong ans.
-    if(p0.x == p1.x) return 0;
-    if(p0.y == p1.y) return 90;
-    
+    if(p0.x == p1.x) return 90;
+    if(p0.y == p1.y) return 0;
+   
     int dy = p1.y - p0.y;
     int dx = p1.x - p0.x;
-    
+   
     float radian_angle = atan((float)dy / dx);
-    
+   
     // If the angle is negative, add π radians (or 180 degrees) to make it positive
     if (radian_angle < 0) radian_angle += PI;
 
@@ -62,12 +60,12 @@ int main() {
     int n;
     cout << "Enter a no. of points :\n";
     cin >> n;
-    
+   
     if (n < 3) {
         cout << "Convex hull cannot be computed with less than 3 points." << endl;
         return 0;
     }
-    
+   
     vector<Point> pts(n);
 
     for(int i=0; i<n; i++) {
@@ -83,7 +81,7 @@ int main() {
             if(p0.x > pts[i].x) {
                 p0 = pts[i];
                 ind = i;
-            } 
+            }
         } else if(p0.y > pts[i].y) {
             p0 = pts[i];
             ind = i;
@@ -99,21 +97,21 @@ int main() {
     for(int i=0; i<n; i++) {
         v.push_back({polar_angle(p0, pts[i]), pts[i]});
     }
-    
+   
     sort(v.begin(), v.end(), compareByFirstElement);
     // cout << p0.x << "," << p0.y << endl;
     // for(auto i: v) cout << i.first << " " << i.second.x << "," << i.second.y << endl;
-    
+   
     stack<Point> st;
    // Push the initial points onto the stack.
     st.push(p0);
     st.push(v[0].second);
     st.push(v[1].second);
-    
+   
     Point next_to_top = v[0].second, top = v[1].second;
     // Start the loop from i = 2
     for (int i = 2; i < n; i++) {
-        while (!st.empty() && direction(next_to_top, top, v[i].second) < 0) {
+        while (!st.empty() && direction(next_to_top, top, v[i].second) >= 0) {
             st.pop();
             top = st.top();
             st.pop();
@@ -121,7 +119,7 @@ int main() {
             st.push(top);
         }
         st.push(v[i].second);
-        
+       
         //making top and next_to_top for next iteration.
         top = st.top();
         st.pop();
