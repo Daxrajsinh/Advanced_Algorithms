0b4dfe7e8e70f1aeb2157a5167aa1c657235f18b
Daxrajsinh <107194145+Daxrajsinh@users.noreply.github.com>
2023-10-26 19:51:43 +0530

    Update sumOfSubet_to_Knapsack.cpp

--- lab6/sumOfSubet_to_Knapsack.cpp
+++ lab6/sumOfSubet_to_Knapsack.cpp
@@ -9,69 +9,56 @@ Sem - V
 #include <bits/stdc++.h>
 using namespace std;
 
-bool sumOfSubset(int arr[], int sum, int n, vector<int>& selectedElements) {
-    bool m[n + 1][sum + 1] = {0};
-
-    for (int i = 0; i <= n; i++) {
-        m[i][0] = true;
-    }
-    for (int j = 1; j <= sum; j++) {
-        m[0][j] = false;
-    }
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= sum; j++) {
-            if (j >= arr[i - 1]) {
-                m[i][j] = m[i - 1][j] || m[i - 1][j - arr[i - 1]];
-            } else {
-                m[i][j] = m[i - 1][j];
+bool knapsack(vector<int> p, vector<int> w, int cap, int max_profit) {
+    int n = p.size();
+    vector<vector<int>> dp(n+1, vector<int>(cap+1, 0));
+    
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j<=cap ; j++) {
+            if(j<w[i]) {
+                dp[i][j] = dp[i-1][j];
+            }
+            else {
+                dp[i][j] = max(dp[i-1][j], p[i]+dp[i-1][j-w[i]]);
             }
         }
     }
-
-    if (!m[n][sum]) {
-        return false;  // No subset found
+    
+    if(dp[n][cap] >= max_profit) { // dp[n][cap] will never be greater than max_profit because cap is same as max_profit
+        return true;
     }
-
-    // Trace back to find selected elements
-    int i = n, j = sum;
-    while (i > 0 && j > 0) {
-        if (m[i][j] && !m[i - 1][j]) {
-            selectedElements.push_back(arr[i - 1]);
-            j -= arr[i - 1];
-        }
-        i--;
+    else {
+        return false;
     }
+}
 
-    return true;
+void sum_of_subset(vector<int> arr, int sum) {
+    if(knapsack(arr, arr, sum, sum) == true) {
+        cout << "Subset is present in arr with sum : " << sum << endl;
+    }
+    else {
+        cout << "Subset is not present in arr with sum : " << sum << endl;
+    }
 }
 
 int main() {
-    int n, t;
-    cout << "Provide no. of elements :\n";
+    
+    int n;
+    cout << "Number of element present in array : ";
     cin >> n;
-
-    int arr[n];
-    cout << "Provide an array :\n";
-    for (int i = 0; i < n; i++) {
+    
+    
+    cout << "Enter all element : ";
+    vector<int> arr(n);
+    for(int i=0 ; i<n ; i++) {
         cin >> arr[i];
     }
     
-    cout << "Provide target sum:\n";
-    cin >> t;
+    int sum;
+    cout << "Required sum : ";
+    cin >> sum;
     
-    vector<int> selectedElements;
-
-    if (sumOfSubset(arr, t, n, selectedElements)) {
-        cout << "Yes, it is possible to get the sum.\n";
-        cout << "Selected elements: ";
-        for (int elem : selectedElements) {
-            cout << elem << " ";
-        }
-        cout << endl;
-    } else {
-        cout << "No, we don't have any subset that satisfies the given sum.\n";
-    }
-
+    sum_of_subset(arr, sum);
+    
     return 0;
 }
