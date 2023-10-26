94f7fa481d2fd4d3e6b430938288a4820ff197ab
Daxrajsinh <107194145+Daxrajsinh@users.noreply.github.com>
2023-10-26 19:00:06 +0530

    Update Horspool_algo.cpp

--- lab5/Horspool_algo.cpp
+++ lab5/Horspool_algo.cpp
@@ -14,41 +14,39 @@ int main() {
     string T, P;
     cout << "Enter a string T :\n";
     cin >> T;
-    
+
     cout << "Enter a string pattern P :\n";
     cin >> P;
-    
+
     int n = T.size(), m = P.size();
-    
-    //Pre-Processing Stage :
-    //Creating Shift Table
-    
-    map<int, int> shift;
-    
-    for(int i=0; i<26; i++) {
+
+    // Pre-Processing Stage:
+    // Creating Shift Table
+
+    map<char, int> shift;
+
+    for (int i = 0; i < 256; i++) {
         shift[i] = m;
     }
-    
-    for(int i=0; i<=m-2; i++) {
-        shift[P[i] - 'a'] = (m - 1 - i);
+
+    for (int i = 0; i <= m - 2; i++) {
+        shift[P[i]] = m - 1 - i;
     }
-    
+
     int j = 0, i;
-    bool flag = 0;
-    while(j + m <= n) {
-        if(P[m-1] == T[j+m-1]) {
-            i = m - 2;
-            while(i >= 0 and P[i] == T[j+i]) {
-                i--;
-            }
-            if(i == -1) {
-                cout << "Pattern found at " << j << endl;
-                flag = 1;
-            }
+    bool flag = false;
+    while (j + m <= n) {
+        i = m - 1;
+        while (i >= 0 && P[i] == T[j + i]) {
+            i--;
         }
-        j += shift[T[j+m-1]];
+        if (i == -1) {
+            cout << "Pattern found at " << j << endl;
+            flag = true;
+        }
+        j += shift[T[j + m - 1]];
     }
-    if(! flag)
+    if (!flag)
         cout << "Pattern Not found !";
     return 0;
 }
