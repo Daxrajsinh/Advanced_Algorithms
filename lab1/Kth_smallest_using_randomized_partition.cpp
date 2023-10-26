a2ead9774b05cab649355fa08784cbc226fd691a
Daxrajsinh <107194145+Daxrajsinh@users.noreply.github.com>
2023-10-26 21:50:37 +0530

    Update Kth_smallest_using_randomized_partition.cpp

--- lab1/Kth_smallest_using_randomized_partition.cpp
+++ lab1/Kth_smallest_using_randomized_partition.cpp
@@ -8,62 +8,47 @@ Sem - V
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition(int a[], int p, int r) {
-  int x = a[r];
-  int i = p-1;
-  int j;
-   for(j=p; j<=r-1; j++) {
-      if(a[j] <= x) {
-          i++;
-          if(a[i] != a[j]) {
-              swap(a[i], a[j]);
-          }
-      }
-  }
-  swap(a[i+1], a[r]);
-  return i+1;
+int partition(int arr[], int p, int q){
+    int z = p-1;
+    int pivot = arr[q];
+    for(int j = p; j < q; j++){
+        if(arr[j]<pivot){
+            z++;
+            swap(arr[z],arr[j]);
+        }
+    }
+    swap(arr[z+1],arr[q]);
+    return z+1;
 }
 
-
-
-
-int randomized_partition(int a[], int p, int r) {
-  srand(time(NULL));
-  int i = p + (rand() % (r - p + 1)); //range from p to r.
-  swap(a[i], a[r]);
-  return partition(a, p, r);
+int randomize_partition(int arr[],int p, int q){
+    int t = p + rand()%(q-p+1);
+    swap(arr[t],arr[q]);
+    return(partition(arr,p,q));
 }
 
-
-
-
-int findkthsmallest(int a[], int p, int r, int k) {
-  if(p < r) {
-      int q = randomized_partition(a,p,r);
-      if(q == k) return a[q-1];
-      if(q > k) {
-          findkthsmallest(a, p, q-1, k);
-      } else {
-          findkthsmallest(a, q+1, r, k);
-      }
-  }
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
-
-int main() {
-   int n;
-   cin >> n;
-  
-   int a[n];
-  
-   for(int i=0; i<n; i++) {
-       cin >> a[i];
-   }
-  
-   int k;
-   cin >> k;
-  
-   cout << findkthsmallest(a, 0, n-1, k);
- 
-   return 0;
+int find(int arr[], int p, int r, int k){
+    if(k > 0 && k <= r - p + 1){
+        int q = randomize_partition(arr,p,r);
+        if(k-1 == q-p){
+            return arr[q];
+        }
+        if(k-1 < q-p){
+            return find(arr,p,q-1,k);
+        }
+        return find(arr,q+1,r,k+p-q-1);
+    }
+    return INT_MAX;
 }
+ int main(){
+    srand(time(NULL));
+    int arr[20] = {10,40,30,50,70,20,90,80,100,60,110,150,120,160,130,170,140,180,200,190};
+    cout<<find(arr,0,19,11);
+    return 0;
+ }
